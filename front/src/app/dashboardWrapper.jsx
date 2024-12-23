b095764aa56cee3abab9bfcfcb015f0e44068ab0
Kaushall-raut <KaushalRaut.code@gmail.com>
2024-12-23 09:10:10 +0530

    added navbar and images for the website

--- front/src/app/dashboardWrapper.jsx
+++ front/src/app/dashboardWrapper.jsx
@@ -1,9 +1,11 @@
-import { Navbar } from "./(components)/Navbar";
+import { Navbar } from "./(components)/Navbar"; //! parenthesis shows that this is not a seperate page
+import { Sidebar } from "./(components)/Sidebar";
 
 export const DashboardWrapper = ({ children }) => {
   //!   children props shows that whatever content you put inside a component will be rendered . in simple terms it  is like props but with some special feature
   return (
     <div className="flex min-h-screen w-full bg-gray-50 text-gray-900 ">
+      <Sidebar />
       <main
         className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64`}
       >
